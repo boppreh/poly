802880d57d1ca6fe60597af0c647823deb980f3a
BoppreH <boppreh@gmail.com>
2014-07-10 14:51:56 -0300

    Create test harness

--- poly.py
+++ poly.py
@@ -1,10 +1,11 @@
 import math
 
+def generate(sequential_points, a):
+    yield 1
+
 def eval2(n0, n1, c, at):
     return at * n1 - (at - 1) * n0 + math.factorial(at) * c
 
-poly = lambda x: 2 * x ** 2 - 3 * x + 5
-for i in range(1, 100):
-    print(i, poly(i), eval2(poly(i-2), poly(i-1), 2, 2))
-
-
+if __name__ == '__main__':
+    from tests import *
+    unittest.main()
